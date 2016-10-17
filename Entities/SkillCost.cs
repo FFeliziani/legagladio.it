07dc06caec960b617c46f3599e9fb2db084b053c
Federico Feliziani <me@federicofeliziani.com>
2016-10-17 12:01:45 +0200

    Added missing files

---
+++ Entities/SkillCost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegaGladio.Entities
+{
+    [Serializable]
+    public enum SkillCost
+    {
+        NOT_SET = 0,
+        NORMAL = 1,
+        DOUBLE = 2
+    }
+}
