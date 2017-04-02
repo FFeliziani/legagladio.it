7f0c228924773a35f5843186bb7b89e89f0919a2
Federico Feliziani <f3l1x@tiscali.it>
2017-04-02 15:17:13 +0200

    switched to sqlserver

--- DataAccessLayer/LegaGladioDS.cs
+++ DataAccessLayer/LegaGladioDS.cs
@@ -1,21 +1,8 @@
 ﻿namespace DataAccessLayer
 {
-}
-
-namespace DataAccessLayer
-{
-}
-namespace DataAccessLayer
-{
 
 
-    public partial class LegaGladioDS
+    partial class LegaGladioDS
     {
-    }
-}
-namespace DataAccessLayer {
-    
-    
-    public partial class LegaGladioDS {
     }
 }
--- DataAccessLayer/Properties/Settings.Designer.cs
+++ DataAccessLayer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DataAccessLayer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.0.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,11 +26,21 @@ namespace DataAccessLayer.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("server=192.168.1.5;user id=legagladio;password=gcipRg5OECMzbi7W9LUfEbfzMHTV8wmL;d" +
-            "atabase=legagladio")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\sqlexpress;Initial Catalog=Legagladio;Integrated Security=True")]
         public string legagladioConnectionString2 {
             get {
                 return ((string)(this["legagladioConnectionString2"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LEGAGLADIO1;Initial Catalog=Legagladio;Persist Security Info=True;Use" +
+            "r ID=Legagladio;Password=gcipRg5OECMzbi7W9LUfEbfzMHTV8wmL")]
+        public string LegagladioConnectionString {
+            get {
+                return ((string)(this["LegagladioConnectionString"]));
             }
         }
     }
